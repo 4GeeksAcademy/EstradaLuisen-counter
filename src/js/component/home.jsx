664bebeccf9e057f89548ce3a6ad0c28b939d457
EstradaLuisen <143045575+EstradaLuisen@users.noreply.github.com>
2024-06-12 06:47:25 +0000

    ADD the timer Interval and the functions to change divs contents

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, {useState, useEffect} from "react";
 const Home = () => {
+	const [seconds , setSeconds]= useState(0);
+	const [cont1 , setCont1]= useState(0);
+	const [cont2 , setCont2]= useState(0);
+	const [cont3 , setCont3]= useState(0);
+	const [cont4 , setCont4]= useState(0);
+	const [cont5 , setCont5]= useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+		  setSeconds(seconds => seconds + 1);
+		}, 1000);
+		
+		  return () => clearInterval(interval);
+		}, []);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center mt-5">
+			<div className="d-flex justify-content-center col-8">
+				<div className="display-5 me-5">
+					<i className="far fa-clock"></i>
+				</div>
+				<div className=" rounded border border-dark display-5 me-2">
+					{cont5}
+				</div>
+				<div className="rounded border border-dark display-5 me-2">
+					{cont4< 10 ? cont4 : (setCont4(0) , setCont5(cont5+1))}
+				</div>
+				<div className="rounded border border-dark display-5 me-2">
+					{cont3< 10 ? cont3 : (setCont3(0) , setCont4(cont4+1))}					
+				</div>
+				<div className="rounded border border-dark display-5 me-2">
+					{cont2< 10 ? cont2 : (setCont2(0) , setCont3(cont3+1))}
+				</div>
+				<div className="rounded border border-dark display-5 me-2">
+					{cont1< 10 ? cont1 : (setCont1(0) , setCont2(cont2+1))}	
+				</div>
+				<div className="rounded border border-dark display-5">
+					{seconds< 10 ? seconds : (setSeconds(0) , setCont1(cont1+1))}
+				</div>
+			</div>
 		</div>
 	);
 };
